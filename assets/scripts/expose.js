ba36698de7bbd5e0ba9489f810b08be6a6d75750
trn019 <trn019@ucsd.edu>
2025-05-06 16:13:13 -0700

    changing image feature done

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,21 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  // implementing changing the image for the different horns
+  const hornSelect = document.getElementById('horn-select');
+  hornSelect.addEventListener('change', (event) => {
+    const selectHorn = event.target.value;
+    const hornImage = document.querySelector('img');
+    hornImage.src = `assets/images/${selectHorn}.svg`;
+    // console.log(`Change image to ${selectHorn}`);
+    // alert(hornImage.src); 
+  });
+
+
+  // implementing the change in icon for the different volumns
+
+  
+  // implementing the sound based on the changes in the volume and the horns
+
+
 }
