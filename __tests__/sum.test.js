eda12a03217171f2cffb9d8f415455d622d8b2ff
trn019 <trn019@ucsd.edu>
2025-05-06 23:07:43 -0700

    fixed sum error

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,10 +2,10 @@
 
 test('adds 1 + 2 to equal 3', () => {
   // TODO
-  expect(1+2).toBe(4);
+  expect(1+2).toBe(3);
 });
 
 import { sum } from '../code-to-unit-test/sum';
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1,2)).toBe(4);
+  expect(sum(1,2)).toBe(3);
 });
