2d54b7803c02e9e06f669bab6cd6a390a91dc11c
trn019 <trn019@ucsd.edu>
2025-05-06 22:08:49 -0700

    smile open animation

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,11 +4,10 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-  
+  // implementing populating the select options
   function populateVoiceSelect() {
-    
-    const voiceSelect = document.getElementById('voice-select');
     const voiceCollection = speechSynthesis.getVoices();
+    const voiceSelect = document.getElementById('voice-select');
     // alert(voiceCollection.length);
     for(let i = 0; i < voiceCollection.length; i++) {
       const option = document.createElement('option');
@@ -24,8 +23,40 @@ function init() {
       
     }
   }  
+    
     populateVoiceSelect();
     if (speechSynthesis.onvoiceschanged !== undefined) {
       speechSynthesis.onvoiceschanged = populateVoiceSelect;
     }
+
+    const voiceSelect = document.getElementById('voice-select');
+    // implementing the text to speech based on the selected element
+    const talkButton = document.querySelector('button');
+    const textSpeech = document.getElementById('text-to-speak');
+
+    talkButton.addEventListener('click', (event) => {
+      const voiceCollection = speechSynthesis.getVoices();
+      const utterText = new SpeechSynthesisUtterance(textSpeech.value);
+      const selectOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+      for (let i = 0; i < voiceCollection.length; i++) {
+        if (selectOption === voiceCollection[i].name) {
+          utterText.voice = voiceCollection[i];
+        }
+      }
+
+      //implenting month open while speaking
+      speechSynthesis.speak(utterText);
+      const imgFace = document.querySelector('img');
+      utterText.onstart =() =>{
+        imgFace.src = 'assets/images/smiling-open.png';
+      };
+      utterText.onend =() =>{
+        imgFace.src = 'assets/images/smiling.png';
+      };
+
+    });
+
+
+
+
 }
