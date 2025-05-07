3c4066804818664f43a2da91c7799475c9ed3354
trn019 <trn019@ucsd.edu>
2025-05-06 19:46:18 -0700

    completed populating the voice select

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,28 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  
+  function populateVoiceSelect() {
+    
+    const voiceSelect = document.getElementById('voice-select');
+    const voiceCollection = speechSynthesis.getVoices();
+    // alert(voiceCollection.length);
+    for(let i = 0; i < voiceCollection.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voiceCollection[i].name} (${voiceCollection[i].lang})`;
+      
+      if (voiceCollection[i].default) {
+        option.textContent += ' - DEFAULT';
+      }
+
+      option.setAttribute('data-lang', voiceCollection[i].lang);
+      option.setAttribute('data-name', voiceCollection[i].name);
+      voiceSelect.appendChild(option);
+      
+    }
+  }  
+    populateVoiceSelect();
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+      speechSynthesis.onvoiceschanged = populateVoiceSelect;
+    }
 }
